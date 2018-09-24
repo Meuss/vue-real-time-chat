d4364d692d28519597e356058ff2c3486af4f912
Meuss <thomas.miller147@gmail.com>
2018-09-24 22:11:14 +0200

    functionality done

---
+++ src/components/Welcome.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="orange-text">Welcome</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name:</label>
+          <input type="text" name="name" v-model="name">
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
+          <button class="btn orange">Enter Chat</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data() {
+    return {
+      name: null,
+      feedback: null,
+    };
+  },
+  methods: {
+    enterChat() {
+      if (this.name) {
+        this.$router.push({ name: 'Chat', params: { name: this.name } });
+      } else {
+        this.feedback = "We don't accept Anons here 😉";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.welcome {
+  max-width: 400px;
+  margin-top: 100px;
+}
+.welcome h2 {
+  font-size: 3em;
+}
+.welcome button {
+  margin: 30px auto;
+}
+</style>
